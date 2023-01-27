52443895810368c03d944bbfc6e92ac472c6384d
fenomenogr777 <nc93gate7@hotmail.com>
2023-01-27 14:02:25 +0200

    create css queries

--- scripts/script.js
+++ scripts/script.js
@@ -29,19 +29,10 @@ class AppCL {
     e.preventDefault();
     const newTask = newTaskInputEL.value;
     if (!newTask) return;
-
-    const html = ` 
-  <div class="list" id="${this.tasks.length}">
-  <input type="text" class="text" value="${newTask}" readonly />
-  <div class="actions">
-  <button class="btn edit">Edit</button>
-  <button class="btn delete">Delete</button>
-  </div>
-  </div>`;
-    listContainer.insertAdjacentHTML("afterbegin", html);
+    this.showTasks(this.tasks.length, newTask);
     newTaskInputEL.value = "";
     this.tasks.push(newTask);
-    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    this.setLocaleStorage(this.tasks);
   }
 
   ///////////////////// DELETE TASK ///////////////////
@@ -51,7 +42,7 @@ class AppCL {
     const id = clicked.closest(".list").getAttribute("id");
     this.tasks.splice(id, 1);
     clicked.closest(".list").remove();
-    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    this.setLocaleStorage(this.tasks);
   }
 
   ///////////////////// EDIT TASK ///////////////////
@@ -72,29 +63,19 @@ class AppCL {
       clicked.innerText = "Edit";
       edit.style.color = " #212529";
       clicked.style.background = "#4c6ef5";
-      const id=clicked.closest(".list").getAttribute("id")
-      console.log(edit,edit.value,id)
+      const id = clicked.closest(".list").getAttribute("id");
       this.tasks[+id] = edit.value;
-      localStorage.setItem("tasks", JSON.stringify(this.tasks));
-      console.log(this.tasks);
+      this.setLocaleStorage(this.tasks);
     }
   }
 
   ///////////////////// LOCALE STORAGE GET ///////////////////
   localStorage() {
     const data = JSON.parse(localStorage.getItem("tasks")) || [];
-    console.log(data);
     this.tasks = data;
     this.tasks.forEach((task, i) => {
       if (task == null) return;
-      const html = ` <div class="list" id="${i}">
-      <input type="text" class="text" value="${task}" readonly />
-      <div class="actions">
-      <button class="btn edit">Edit</button>
-      <button class="btn delete">Delete</button>
-      </div>
-      </div>`;
-      listContainer.insertAdjacentHTML("afterbegin", html);
+      this.showTasks(i, task);
     });
   }
 
@@ -104,6 +85,21 @@ class AppCL {
     titleHead = `To Do List ${currentYear}`;
     titleEL.textContent = `To Do List ${currentYear}`;
     footerYearEL.textContent = `${currentYear}`;
+  }
+
+  showTasks(id, value) {
+    const html = ` 
+    <div class="list" id="${id}">
+    <input type="text" class="text" value="${value}" readonly />
+    <div class="actions">
+    <button class="btn edit">Edit</button>
+    <button class="btn delete">Delete</button>
+    </div>
+    </div>`;
+    listContainer.insertAdjacentHTML("afterbegin", html);
+  }
+  setLocaleStorage(value) {
+    localStorage.setItem("tasks", JSON.stringify(value));
   }
 }
 
